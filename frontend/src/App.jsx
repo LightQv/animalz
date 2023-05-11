d039f95f93f856f70ee4b40c8d9ae9060fdc0ea7
LightQv <QVivian23@gmail.com>
2023-05-11 16:22:57 +0200

    NavBar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import ProfilesCard from "./pages/ProfilesCard";
 import ProfileDetails from "./pages/ProfileDetails";
@@ -7,6 +8,7 @@ import ProfileDetails from "./pages/ProfileDetails";
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profiles-list" element={<ProfilesCard />} />
