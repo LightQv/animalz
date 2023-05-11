b4e2d471f37d04f30ef9758b40728adf4c6ccca8
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-05-11 14:11:38 +0200

    home_page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.homePage}>
+      <img
+        src="../src/assets/logo/Animalz.png"
+        alt="logo"
+        className={styles.logo}
+      />
+      <div className={styles.buttons}>
+        <button type="button" className={styles.button}>
+          Register
+        </button>
+        <button type="button" className={styles.button}>
+          Log in
+        </button>
+      </div>
+    </div>
+  );
 }
