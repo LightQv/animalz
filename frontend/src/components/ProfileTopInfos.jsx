b6ba2d656cbaf4a0715c5f097b552bf9565b3473
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-05-11 18:11:10 +0200

    cover picture

--- frontend/src/components/ProfileTopInfos.jsx
+++ frontend/src/components/ProfileTopInfos.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+import { useFetchContext } from "../contexts/FetchContext";
 import styles from "./ProfileTopInfos.module.css";
 
 export default function ProfileTopInfos() {
+  const { images } = useFetchContext();
+  //  const url = "https://images.pexels.com/photos/";
+
   return (
-    <div className={styles.topInfos}>
-      <img src="" alt="profile" />
-      <div className={styles.mainInfos}>
-        <h2>Name, age</h2>
-        <div className={styles.animalsInfos}>
-          <img src="" alt="travel animal" />
-          <img src="" alt="climate animal" />
+    <div
+      className={styles.coverPicture}
+      style={{
+        backgroundImage: `url(${images.src.tiny})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "bottom",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className={styles.topInfos}>
+        <img src="" alt="profile" />
+        <div className={styles.mainInfos}>
+          <h2>Name, age</h2>
+          <div className={styles.animalsInfos}>
+            <img src="../src/assets/images/chat-noir.png" alt="travel animal" />
+            <img
+              src="../src/assets/images/004-silhouette-vue-ct-ours.png"
+              alt="climate animal"
+            />
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/contexts/FetchContext.jsx
+++ frontend/src/contexts/FetchContext.jsx
@@ -1,15 +1,30 @@
+import axios from "axios";
 import PropTypes from "prop-types";
-import { createContext, useState, useMemo, useContext } from "react";
+import { createContext, useState, useMemo, useContext, useEffect } from "react";
 
 const FetchContext = createContext();
 
 export default FetchContext;
+
+const API_KEY = import.meta.env.VITE_PEXELS_API_KEY;
 
 export function FetchContextProvider({ children }) {
   // States
   const [usersInfos, setUsersInfos] = useState([]);
   const [randomUsers, setRandomUsers] = useState([]);
   const [images, setImages] = useState([]);
+
+  const getImages = () => {
+    axios("https://api.pexels.com/v1/search?query=voyage", {
+      headers: {
+        Authorization: `${API_KEY}`,
+      },
+    })
+      .then((data) => {
+        setImages(data.data.photos[1]);
+      })
+      .catch((err) => console.error(err));
+  };
 
   // Memo pour optimisation => empêche les rerenders intempestifs au moindre changement de state
   //  - Passer les getter et setter de vos states entre les accolades, et le getter dans le tableau
@@ -25,6 +40,7 @@ export function FetchContextProvider({ children }) {
     }),
     [usersInfos, randomUsers, images]
   );
+  useEffect(getImages, []);
 
   return (
     <FetchContext.Provider value={value}>{children}</FetchContext.Provider>
