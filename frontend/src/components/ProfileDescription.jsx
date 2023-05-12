15de651d545605a3d7a023bba0ccc9da53360e43
LightQv <QVivian23@gmail.com>
2023-05-12 11:27:21 +0200

    usercard css

--- frontend/src/components/ProfileDescription.jsx
+++ frontend/src/components/ProfileDescription.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import liked from "../assets/icons/liked.png";
+import favorite from "../assets/icons/favorite.png";
 import styles from "./ProfileDescription.module.css";
 import { useFetchContext } from "../contexts/FetchContext";
 
@@ -26,57 +28,103 @@ export default function ProfileDescription() {
   return (
     usersInfos && (
       <div className={styles.description}>
-        <h3 className={styles.categoryTitle}>Description</h3>
-        <p className={styles.location}>
-          {usersInfos[id].location
-            ? usersInfos[id].location.city
-            : randomUsers[id].location.city}
-          ,{" "}
-          {usersInfos[id].location
-            ? usersInfos[id].location.country
-            : randomUsers[id].location.country}
-        </p>
-        <h4>
-          <span>I speak</span>{" "}
-          {language.charAt(0).toUpperCase() + language.slice(1).toLowerCase()}
-        </h4>
-        <div>
+        <section>
+          <h3 className={styles.categoryTitle}>Description</h3>
+          <p className={styles.location}>
+            {usersInfos[id].location
+              ? usersInfos[id].location.city
+              : randomUsers[id].location.city}
+            ,{" "}
+            {usersInfos[id].location
+              ? usersInfos[id].location.country
+              : randomUsers[id].location.country}
+          </p>
+          <h4>
+            <span>I speak</span>{" "}
+            {language.charAt(0).toUpperCase() + language.slice(1).toLowerCase()}
+          </h4>
           <div>
-            <h5>I'm interested in :</h5>
-            <ul>
-              {culuralInterests.map((el) => (
-                <li key={el}>
-                  {el.charAt(0).toUpperCase()}
-                  {el.slice(1).toLowerCase()}
-                </li>
-              ))}
-            </ul>
+            <div>
+              <h5>I'm interested in :</h5>
+              <ul>
+                {culuralInterests.map((el) => (
+                  <li key={el}>
+                    {el.charAt(0).toUpperCase()}
+                    {el.slice(1).toLowerCase()}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h5>I'd like to visit :</h5>
+              <ul>
+                {geoInterests.map((el) => (
+                  <li key={el}>
+                    {el.charAt(0).toUpperCase()}
+                    {el.slice(1).toLowerCase()}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h5>I want to travel by :</h5>
+              <ul>
+                {locomotion.map((el) => (
+                  <li key={el}>
+                    {el.charAt(0).toUpperCase()}
+                    {el.slice(1).toLowerCase()}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <h5>My budget : {`${budget[0]} - ${budget[1]} €`}</h5>
+            <h5>Availability for travel : {travelDuration} days</h5>
           </div>
-          <div>
-            <h5>I'd like to visit :</h5>
-            <ul>
-              {geoInterests.map((el) => (
-                <li key={el}>
-                  {el.charAt(0).toUpperCase()}
-                  {el.slice(1).toLowerCase()}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h5>I want to travel by :</h5>
-            <ul>
-              {locomotion.map((el) => (
-                <li key={el}>
-                  {el.charAt(0).toUpperCase()}
-                  {el.slice(1).toLowerCase()}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <h5>My budget : {`${budget[0]} - ${budget[1]} €`}</h5>
-          <h5>Availability for travel : {travelDuration} days</h5>
-        </div>
+        </section>
+        {id !== "0" ? (
+          <section className={styles.likeFavButtons}>
+            {id !== "0" ? (
+              <button
+                type="button"
+                onClick={() => {
+                  if (!usersInfos[0].liked_ids.includes(id)) {
+                    usersInfos[0].liked_ids.push(id);
+                  } else {
+                    usersInfos[0].liked_ids.splice(
+                      usersInfos[0].liked_ids.indexOf(id),
+                      1
+                    );
+                  }
+                }}
+                className={styles.likeButton}
+              >
+                <img className={styles.liked} src={liked} alt="liked" />
+              </button>
+            ) : null}
+            {id !== "0" ? (
+              <button
+                type="button"
+                onClick={() => {
+                  if (!usersInfos[0].favorite_ids.includes(id)) {
+                    usersInfos[0].favorite_ids.push(id);
+                  } else {
+                    usersInfos[0].favorite_ids.splice(
+                      usersInfos[0].favorite_ids.indexOf(id),
+                      1
+                    );
+                  }
+                }}
+                className={styles.favButton}
+              >
+                <img
+                  className={styles.favorite}
+                  src={favorite}
+                  alt="favorite"
+                />
+              </button>
+            ) : null}
+          </section>
+        ) : null}
       </div>
     )
   );
--- frontend/src/components/ProfileTopInfos.jsx
+++ frontend/src/components/ProfileTopInfos.jsx
@@ -10,8 +10,6 @@ import bear from "../assets/images/004-silhouette-vue-ct-ours.png";
 import frog from "../assets/images/002-frop-tropical.png";
 import lion from "../assets/images/001-lion.png";
 import fox from "../assets/images/003-renard-assis.png";
-import liked from "../assets/icons/liked.png";
-import favorite from "../assets/icons/favorite.png";
 import lucie from "../assets/images/Lucie.jpg";
 
 export default function ProfileTopInfos() {
@@ -48,42 +46,6 @@ export default function ProfileTopInfos() {
         className={styles.coverPicture}
       />
       <div className={styles.topInfos}>
-        {id !== "0" ? (
-          <button
-            type="button"
-            onClick={() => {
-              if (!usersInfos[0].liked_ids.includes(id)) {
-                usersInfos[0].liked_ids.push(id);
-              } else {
-                usersInfos[0].liked_ids.splice(
-                  usersInfos[0].liked_ids.indexOf(id),
-                  1
-                );
-              }
-            }}
-          >
-            Like
-            <img className={styles.liked} src={liked} alt="liked" />
-          </button>
-        ) : null}
-        {id !== "0" ? (
-          <button
-            type="button"
-            onClick={() => {
-              if (!usersInfos[0].favorite_ids.includes(id)) {
-                usersInfos[0].favorite_ids.push(id);
-              } else {
-                usersInfos[0].favorite_ids.splice(
-                  usersInfos[0].favorite_ids.indexOf(id),
-                  1
-                );
-              }
-            }}
-          >
-            Favorite
-            <img className={styles.favorite} src={favorite} alt="favorite" />
-          </button>
-        ) : null}
         <img
           src={`${id === "0" ? lucie : randomUsers[id].picture.large}`}
           alt="profile-pic"
