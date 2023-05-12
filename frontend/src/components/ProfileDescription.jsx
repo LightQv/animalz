5e59b714a76c4f7783983050a34b114663ea333b
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-05-12 10:26:53 +0200

    card leaflet

--- frontend/src/components/ProfileDescription.jsx
+++ frontend/src/components/ProfileDescription.jsx
@@ -3,14 +3,16 @@ import styles from "./ProfileDescription.module.css";
 
 export default function ProfileDescription() {
   return (
-    <div className={styles.description}>
-      <h3>City, Country</h3>
-      <h4>Speaks: language</h4>
-      <h5>Activities I'm interested in: cultural_interests</h5>
-      <h5>Places I want to visit: geography_interest</h5>
-      <h5>How I want to travel: locomotion</h5>
-      <h5>My budget: budget</h5>
-      <h5>Availability for travel: travel_duration</h5>
+    <div>
+      <div className={styles.description}>
+        <h3>City, Country</h3>
+        <h4>Speaks: language</h4>
+        <h5>Activities I'm interested in: cultural_interests</h5>
+        <h5>Places I want to visit: geography_interest</h5>
+        <h5>How I want to travel: locomotion</h5>
+        <h5>My budget: budget</h5>
+        <h5>Availability for travel: travel_duration</h5>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,6 @@ export default function Home() {
   const onClickHandle = () => {
     setShowModal(true);
   };
-
   return (
     <div className={styles.homePage}>
       <AnimalChoices showModal={showModal} />
